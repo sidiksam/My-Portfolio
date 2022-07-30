a6f7b68d78900b4ee5d0ed7c94f48c544ef99404
sidiksam <bakish76@gmail.com>
2022-07-30 20:56:21 +0000

    validation

--- script.js
+++ script.js
@@ -14,27 +14,69 @@ function closeMenu() {
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
-// Validation
 
-const form = document.getElementById('formSection');
-const errorDisplay = document.getElementById('errorDisplay');
+// form validation
 
-function formSubmit() {
-  const email = document.getElementById('email').value;
-  const checker = /^([a-z0-9.-]+)@([a-z0-9-]+).([a-z]{2,20})(.[a-z]{2,8})?$/;
- 
+// show a message with a type of the input
+function showMessage(input, message, type) {
+  // get the small element and set the message
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? 'success' : 'error';
+  return type;
+}
 
-  form.addEventListener('submit', (e) => {
-    if (email.value.match(checker)) {
-      e.preventDefault();
-      errorDisplay.style.visibility = 'visible';
-      errorDisplay.classList.add('error-message');
-      errorDisplay.textContent = '*Your email address should be all in lowercase';
-    } else {
-      errorDisplay.style.visibility = 'hidden';
-    }
-  });
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
 
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+const form = document.querySelector('#signup');
+
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const EMAIL_INVALID = 'Please enter a correct email address format';
+
+form.addEventListener('submit', (e) => {
+  // stop form submission
+  e.preventDefault();
+
+  // validate the form
+  const nameValid = hasValue(form.elements.name, NAME_REQUIRED);
+  const emailValid = validateEmail(form.elements.email, EMAIL_REQUIRED, EMAIL_INVALID);
+  // if valid, submit the form.
+  if (nameValid && emailValid) {
+      
+  }
+});
+
+//
 const mobileModalContent = [
   {
     title: 'Multi-Post Stories',
